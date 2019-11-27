cad14ceae27cf1e9e2f8c347a8fb8c7fd2868ae0
Александр Поляков <backstabe@gmail.com>
2019-11-27 22:34:24 +0300

    docs: params

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -2,13 +2,13 @@ class Phrase
   attr_reader :words
   # Given a phrase, count the occurrences of each word in that phrase.
   # For example for the input `"olly olly in come free"`
-
-  # @params [String]
+  #
+  # @param [String] words
   def initialize(words)
     @words = words.scan(/\b[\w']+\b/)
   end
 
-  # @return [Hash]
+  # @return [Hash] { 'word' => number }
   def word_count
     words.group_by(&:downcase).transform_values(&:size)
   end
