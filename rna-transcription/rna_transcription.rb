91d62e6cf13c615c45f4b752f47a41543e65bbfa
Александр Поляков <backstabe@gmail.com>
2019-12-01 02:06:18 +0300

    feature: task rna-transcription

---
+++ rna-transcription/rna_transcription.rb
@@ -0,0 +1,29 @@
+class Complement
+  DNA = 'CATG'
+  RNA = 'GUAC'
+
+  # Given a DNA strand, return its RNA complement (per RNA transcription).
+  #
+  # Both DNA and RNA strands are a sequence of nucleotides.  
+  #
+  # The four nucleotides found in DNA are adenine (**A**), cytosine (**C**),
+  # guanine (**G**) and thymine (**T**).
+  #
+  # The four nucleotides found in RNA are adenine (**A**), cytosine (**C**),
+  # guanine (**G**) and uracil (**U**).  
+  # 
+  # Given a DNA strand, its transcribed RNA strand is formed by replacing
+  # each nucleotide with its complement: 
+  #
+  # * `G` -> `C`
+  # * `C` -> `G`
+  # * `T` -> `A`
+  # * `A` -> `U`
+  #
+  # @param dna [String]
+  #
+  # @return [String] RNA
+  def self.of_dna(dna)
+    dna.tr(DNA, RNA)
+  end
+end
